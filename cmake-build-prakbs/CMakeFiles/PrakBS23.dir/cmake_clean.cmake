34f6605b2694e6d68e986bd91022f8cb896fe418
Widumaster <105982273+Widumaster@users.noreply.github.com>
2023-04-01 12:03:06 +0200

    Default CLion Project
    -Created first project and added to git

---
+++ cmake-build-prakbs/CMakeFiles/PrakBS23.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrakBS23.dir/main.c.o"
+  "PrakBS23.pdb"
+  "PrakBS23"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrakBS23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
