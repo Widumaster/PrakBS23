55e1063baae93cad98a682df714590e804e1c5a4
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2023-04-26 15:19:29 +0200

    MultiClient

--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
 
 #define BUFSIZE 1024 // Größe des Buffers
 #define TRUE 1
 #define ENDLOSSCHLEIFE 1
 #define PORT 5678
+#define NUM_OF_CHILDS 4
+#define SEGSIZE sizeof(KeyValue)
 
 int checkKey(int key);
 
@@ -108,13 +112,24 @@ int delFun(int cfd, int bytes_read, char *in, int *keyHolder, int *valueHolder) 
 }
 
 int main() {
-    int rfd; // Rendevouz-Descriptor
+    int rfd; // Rendevouz-Descriptor/ File-Descriptor
     int cfd; // Verbindungs-Descriptor
 
     struct sockaddr_in client; // Socketadresse eines Clients
     socklen_t client_len; // Länge der Client-Daten
     char in[BUFSIZE]; // Daten vom Client an den Server
-    int bytes_read; // Anzahl der Bytes, die der Client geschickt hat
+    int bytes_read = 1; // Anzahl der Bytes, die der Client geschickt hat
+
+    int i, id, *shar_mem;   /*  id für das Shared Memory Segment        */
+    /*  mit *shar_mem kann der im Shared Memory */
+    /*  gespeicherte Wert verändert werden      */
+    int pid[NUM_OF_CHILDS]; /*  enthält die PIDs der Kindprozesse       */
+
+    /* Nun wird das Shared Memory Segment angefordert, an den Prozess   */
+    /* angehängt, und der dort gespreicherte Wert auf 0 gesetzt         */
+    id = shmget(IPC_PRIVATE, SEGSIZE, IPC_CREAT | 0600);
+    shar_mem = (int *) shmat(id, 0, 0);
+    *shar_mem = 0;
 
 
     // Socket erstellen
@@ -132,10 +147,10 @@ int main() {
 
     // Socket binden
     struct sockaddr_in server;
-    server.sin_family = AF_INET;
+    server.sin_family = AF_INET; //IPv4
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(PORT);
-    int brt = bind(rfd, (struct sockaddr *) &server, sizeof(server));
+    server.sin_port = htons(PORT); //Port
+    int brt = bind(rfd, (struct sockaddr *) &server, sizeof(server)); // Socket an eine Adresse binden
     if (brt < 0) {
         fprintf(stderr, "socket konnte nicht gebunden werden\n");
         exit(-1);
@@ -153,40 +168,83 @@ int main() {
     int valueHolder;
 
     while (ENDLOSSCHLEIFE) {
-
         // Verbindung eines Clients wird entgegengenommen
         cfd = accept(rfd, (struct sockaddr *) &client, &client_len);
-
-        // Lesen von Daten, die der Client schickt
-        bytes_read = read(cfd, in, BUFSIZE);
-
-        // Zurückschicken der Daten, solange der Client welche schickt (und kein Fehler passiert)
-        while (bytes_read > 0) {
-            printf("sending back the %d bytes I received...\n", bytes_read);
-
+        int pidD = fork();
+        if (pidD != 0) {
+            printf("After Forking \n");
+            // Lesen von Daten, die der Client schickt
             bytes_read = read(cfd, in, BUFSIZE);
-            //PUT
-            if (strncmp(in, "put", 3) == 0) {
-                putFun(cfd, bytes_read, in, &keyHolder, &valueHolder);
-            //GET
-            } else if (strncmp(in, "get", 3) == 0) {
-                getFun(cfd, bytes_read, in, &keyHolder, &valueHolder);
-            }
-            //DEL
-            else if (strncmp(in, "del", 3) == 0) {
-                delFun(cfd, bytes_read, in, &keyHolder, &valueHolder);
-            } else if (strncmp(in, "quit", 4) == 0) {
-                printf("Verbindung abgebrochen\n");
-                close(cfd);
-            } else {
-                printf("Fehlgeschlagen \n");
-            }
+            printf("After Client Input \n");
 
+            // Zurückschicken der Daten, solange der Client welche schickt (und kein Fehler passiert)
+            while (bytes_read > 0) {
+                printf("sending back the %d bytes I received...\n", bytes_read);
+                printf("GETPid: %i \n", getpid());
+                bytes_read = read(cfd, in, BUFSIZE);
+                //PUT
+                if (strncmp(in, "put", 3) == 0) {
+                    //putFun(cfd, bytes_read, in, &keyHolder, &valueHolder);
+                    KeyValue *shar_mem = NULL;
+                    write(cfd, "Key: ", strlen("Key: "));
+                    bytes_read = read(cfd, in, BUFSIZE);
+                    keyHolder = atoi(in);
+
+                    //Get Value
+                    write(cfd, "Value: ", strlen("Value: "));
+                    bytes_read = read(cfd, in, BUFSIZE);
+                    valueHolder = atoi(in);
+                    printf("Put Key %i with Value : %i \n ", keyHolder, valueHolder);
+
+                    put(keyHolder, valueHolder);
+                    //GET
+                } else if (strncmp(in, "get", 3) == 0) {
+                    //getFun(cfd, bytes_read, in, &keyHolder, &valueHolder);
+                    write(cfd, "Key: ", strlen("Key: "));
+                    bytes_read = read(cfd, in, BUFSIZE);
+                    int keyValue = atoi(in);
+                    int keyExists = checkKey(keyValue);
+                    if (keyExists) {
+                        get(keyValue);
+                        char valueStr[BUFSIZE];
+                        sprintf(valueStr, "%d", kv.valueStore[keyValue]);
+                        printf(valueStr);
+                        write(cfd, "Value: ", strlen("Value: "));
+                        write(cfd, valueStr, strlen(valueStr));
+                        write(cfd, "\n", strlen("\n"));
+                    } else {
+                        write(cfd, "Schluessel nicht vorhanden", strlen("Schluessel nicht vorhanden"));
+                        write(cfd, "\n", strlen("\n"));
+                    }
+                }
+                    //DEL
+                else if (strncmp(in, "del", 3) == 0) {
+                    //delFun(cfd, bytes_read, in, &keyHolder, &valueHolder);
+                    write(cfd, "Key: ", strlen("Key: "));
+                    bytes_read = read(cfd, in, BUFSIZE);
+                    int keyValue = atoi(in);
+                    int keyExists = checkKey(keyValue);
+                    if (keyExists) {
+                        del(keyValue);
+                        write(cfd, "Schluessel geloescht\n", strlen("Schluessel geloescht\n"));
+                    } else {
+                        write(cfd, "Schluessel nicht vorhanden\n", strlen("Schluessel nicht vorhanden\n"));
+                    }
+                    //QUIT
+                } else if (strncmp(in, "quit", 4) == 0) {
+                    printf("Verbindung abgebrochen\n");
+                    shmdt(shar_mem);
+                    shmctl(id, IPC_RMID, 0);
+                    close(cfd);
+                } else {
+                    printf("Fehlgeschlagen \n");
+                }
+
+            }
         }
         close(cfd);
     }
 
     // Rendevouz Descriptor schließen
     close(rfd);
-
 }
