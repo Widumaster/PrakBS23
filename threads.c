548804df15c25688fea5fcfb663895f98ae8e17e
Widumaster <105982273+Widumaster@users.noreply.github.com>
2023-04-26 16:45:45 +0200

    FIles for multithreading

---
+++ threads.c
@@ -0,0 +1,8 @@
+//
+// Created by Widumaster on 26.04.2023.
+//
+
+#include "threads.h"
+//Vorbereitung für die Implementation von "pthreads" und "Semaphoren" um von Single thread zu Multithread zu transition
+
+
---
+++ threads.h
@@ -0,0 +1,18 @@
+//
+// Created by Widumaster on 26.04.2023.
+//
+
+#ifndef PRAKBS23_THREADS_H
+#define PRAKBS23_THREADS_H
+
+
+
+
+
+
+
+
+
+
+
+#endif //PRAKBS23_THREADS_H
